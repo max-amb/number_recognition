2e4fc4e83e44ebaedc8d4b45a1e735c6d75b88ff
max-amb <max_a@e.email>
2025-10-23 09:40:24 +0100

    Added benchmarking script for parallelisation

--- benchmarking/comparison_script.py
+++ benchmarking/comparison_script.py
@@ -3,12 +3,14 @@ import matplotlib.pyplot as plt
 def main():
     python_data = pull_data_from_json("./py_results.json")
     rust_data = pull_data_from_json("./rust_results.json")
+    rust_parallel_data = pull_data_from_json("./rust_parallel_results.json")
     fig, ax = plt.subplots(figsize=(10, 100))
     vp1 = ax.hist(python_data["times"])
     vp2 = ax.hist(rust_data["times"])
+    vp3 = ax.hist(rust_parallel_data["times"])
 
     ax.autoscale(enable=True, axis='both', tight=False)
-    ax.set_title("Comparison between execution times of rust version (orange) and python version (blue)", fontsize = 20)
+    ax.set_title("Comparison between execution times", fontsize = 20)
     ax.set_xlabel("Execution time (s)", fontsize = 20)
     ax.set_ylabel("Runs", fontsize = 20)
     plt.show()
