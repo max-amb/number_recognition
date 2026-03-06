e0ef142627ff45b0d0d510be5a51812a14b64114
max-amb <max_a@e.email>
2026-03-06 09:31:39 +0000

    Updated plot colours

--- results/comparison_script.py
+++ results/comparison_script.py
@@ -5,11 +5,12 @@ def main():
     rust_data = pull_data_from_json("./rust_results.json")
     fig, ax = plt.subplots(figsize=(10, 100))
     b1 = ax.hist(parallel["times"], color="red")
-    b2 = ax.hist(rust_data["times"])
+    b2 = ax.hist(rust_data["times"], color="orange")
 
     ax.autoscale(enable=True, axis='both', tight=False)
     ax.set_title("Comparison between execution times of rust version (orange) and rust parallel version (red)")
     ax.xaxis.set_label_text("Execution time (s)")
+    ax.yaxis.set_label_text("Runs")
     plt.show()
 
 def pull_data_from_json(file_path: str) -> dict:
