6752126ee518a70878dc260ecfed7b374a1cdbdb
max-amb <max_a@e.email>
2026-06-22 23:36:11 +0100

    Moved fully connected to store shape and added assert

--- src/layers/fully_connected.rs
+++ src/layers/fully_connected.rs
@@ -1,19 +1,21 @@
 use nalgebra::{DMatrix, DVector};
 
-use crate::layers::{Forward, Matrix};
+use crate::layers::{Forward, Mat};
 
 #[derive(Debug)]
 pub struct FullyConnected {
-    output_size: usize,
+    output_shape: (usize, usize),
     weights: DMatrix<f32>,
     biases: DVector<f32>
 }
 
 impl Forward for FullyConnected {
-    fn run(&self, prev_layer: Matrix) -> Matrix {
-        Matrix {
-            data: &self.weights * prev_layer.data + &self.biases,
-            shape: (self.output_size, 1)
+    fn run(&self, prev_layer: Mat) -> Mat {
+        let data = &self.weights * prev_layer.data + &self.biases;
+        assert_eq!(data.nrows(), self.output_shape.0*self.output_shape.1);
+        Mat {
+            data,
+            shape: self.output_shape
         }
     } 
 }
