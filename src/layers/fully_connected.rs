6432aeb8f12dbc6a97c1b8a97dfcf0ae4055fc17
max-amb <max_a@e.email>
2026-06-25 16:42:39 +0100

    Migrated fully connected to utilise tensors

--- src/layers/fully_connected.rs
+++ src/layers/fully_connected.rs
@@ -1,60 +1,37 @@
-use nalgebra::{DMatrix, DVector};
-use rand::RngExt;
-use rand_distr::{Normal, Distribution};
+use nalgebra::DVector;
 
-use crate::layers::{Forward, Mat};
-use crate::initialisation::{Initialisable, InitialisationOptions};
+use crate::layers::{Forward, Initialisable};
+use crate::initialisation::InitialisationOptions;
+use crate::tensor::{Shape, Ten};
 
 #[derive(Debug)]
 pub struct FullyConnected {
-    output_shape: (usize, usize),
-    weights: Option<DMatrix<f32>>,
-    biases: Option<DVector<f32>>,
+    output_shape: Shape,
+    // Need to be options as we do not know shape at creation
+    weights: Option<Ten>,
+    biases: Option<Ten>,
     initialisation_options: InitialisationOptions
 }
 
 impl FullyConnected {
-    fn new(output_shape: (usize, usize), initialisation_options: InitialisationOptions) -> Self {
+    fn new(output_shape: Shape, initialisation_options: InitialisationOptions) -> Self {
         Self { output_shape, weights: None, biases: None, initialisation_options } 
     }
 }
 
 impl Forward for FullyConnected {
-    fn run(&self, prev_layer: Mat) -> Mat {
-        let data = self.weights.as_ref().unwrap() * prev_layer.data + self.biases.as_ref().unwrap();
-        assert_eq!(data.nrows(), self.output_shape.0 * self.output_shape.1);
-        Mat {
-            data,
-            shape: self.output_shape,
-        }
+    fn run(&self, prev_layer: Ten) -> Ten {
+        let data: Ten = self.weights.as_ref().unwrap() * prev_layer + self.biases.as_ref().unwrap();
+        assert_eq!(data.shape.magnitude(), self.output_shape.magnitude());
+        Ten { data: data.data, shape: self.output_shape }
     }
 }
 
 impl Initialisable for FullyConnected {
-    fn initialise(&mut self, previous_shape: (usize, usize)) -> (usize, usize) {
-        let mut rng = rand::rng();
+    fn initialise(&mut self, previous_shape: Shape) -> Shape {
+        self.weights = Some(self.initialisation_options.create_matrix((self.output_shape.magnitude(), previous_shape.magnitude(), 1).into(), previous_shape.magnitude()));
 
-        self.weights = Some(match &self.initialisation_options {
-            InitialisationOptions::Random => {
-                DMatrix::from_fn(
-                    self.output_shape.0 * self.output_shape.1,
-                    previous_shape.0 * previous_shape.1,
-                    |_, _| rng.random_range(-1.0..=1.0),
-                )
-            }
-            InitialisationOptions::He => {
-                let normal_dist = Normal::new(0.0, (2.0_f32 / ((previous_shape.0*previous_shape.1) as f32)).sqrt()).unwrap();
-                DMatrix::from_fn(
-                    self.output_shape.0 * self.output_shape.1,
-                    previous_shape.0 * previous_shape.1,
-                    |_, _| {
-                        normal_dist.sample(&mut rng)
-                    }
-                )
-            }
-        });
-
-        self.biases = Some(DVector::from_element(self.output_shape.0*self.output_shape.1, 0.0));
+        self.biases = Some(DVector::from_element(self.output_shape.magnitude(), 0.0).into());
         self.output_shape
     }
 }
