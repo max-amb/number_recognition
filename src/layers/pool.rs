c877195422912a14b8b9250301b611a336ef82d9
max-amb <max_a@e.email>
2026-06-27 00:21:54 +0100

    Moved pool to require pool types

--- src/layers/pool.rs
+++ src/layers/pool.rs
@@ -1,28 +1,47 @@
-use nalgebra::{Const, DVector, Dyn};
+use nalgebra::DVector;
 
 use crate::layers::primitives::{im2col, out_shape};
 use crate::layers::{Convolvable, Forward, Initialisable};
 use crate::tensor::{Shape, Ten};
 
 #[derive(Debug)]
+pub enum PoolType {
+    MaxPool,
+    AvgPool
+}
+
+#[derive(Debug)]
 pub struct Pool {
     shape: (usize, usize),
     stride: usize,
-    pooling_function: fn(DVector<f32>) -> f32,
-    output_depth: Option<usize>
+    pool_type: PoolType,
+    output_depth: Option<usize>,
 }
 
 impl Pool {
     fn new(
         shape: (usize, usize),
         stride: usize,
-        pooling_function: fn(DVector<f32>) -> f32,
+        pool_type: PoolType,
     ) -> Self {
         Self {
             shape,
             stride,
-            pooling_function,
+            pool_type,
             output_depth: None
+        }
+    }
+
+    fn pooling_function<I>(&self, values: I) -> f32
+        where I: IntoIterator<Item = f32>
+    {
+        let iterator = values.into_iter();
+        match self.pool_type {
+            PoolType::MaxPool => iterator.fold(f32::NEG_INFINITY, |curr, x| if x > curr {x} else {curr}),
+            PoolType::AvgPool => {
+                let (count, sum) = iterator.fold((0, 0.0), |(curr_iter, curr_sum), x| (curr_iter+1, curr_sum+x));
+                sum/(count as f32)
+            }
         }
     }
 }
@@ -62,7 +81,7 @@ impl Forward for Pool {
         for i in 0..outshape.channels {
             let channel_block = prev_columnised.rows(i*size_of_view, size_of_view);
             for col in channel_block.column_iter() {
-                result.push((self.pooling_function)(col.into()));
+                result.push(self.pooling_function(col.into_iter().copied()));
             }
         }
         Ten {
