e1065cfb17490521e7f293531ffc64e2a4be0b67
max-amb <max_a@e.email>
2026-06-26 15:40:56 +0100

    Moved tests and added conversion

--- src/tensor/tensor.rs
+++ src/tensor/tensor.rs
@@ -18,10 +18,6 @@ impl Ten {
             shape: self.shape,
         }
     }
-
-    pub fn new() -> Self {
-        todo!()
-    }
 }
 
 impl From<DMatrix<f32>> for Ten {
@@ -29,6 +25,18 @@ impl From<DMatrix<f32>> for Ten {
         Self {
             data: DVector::from_vec(mat.into_iter().copied().collect()),
             shape: mat.shape().into(),
+        }
+    }
+}
+
+impl From<Vec<DMatrix<f32>>> for Ten {
+    fn from(matrices: Vec<DMatrix<f32>>) -> Self {
+        assert!(!matrices.is_empty());
+        assert!(matrices.iter().map(|x| x.shape()).all(|x| x == matrices[0].shape()));
+        let output_shape: Shape = (matrices[0].nrows(), matrices[0].ncols(), matrices.len()).into();
+        Self {
+            data: DVector::from_iterator(output_shape.magnitude(), matrices.into_iter().flat_map(|x| x.into_iter().copied().collect::<Vec<f32>>())),
+            shape: output_shape,
         }
     }
 }
@@ -125,5 +133,47 @@ impl std::ops::Mul for Ten {
 
     fn mul(self, rhs: Self) -> Self::Output {
         &self * &rhs
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use hegel::{Generator, TestCase, HealthCheck};
+    use hegel::generators as gs;
+
+    // Need to supress filter too mutch as we have a very restrictive filter
+    #[hegel::test(suppress_health_check = [HealthCheck::FilterTooMuch])]
+    fn test_dmatrix_to_tensor(tc: TestCase) {
+        let vec: Vec<f32> = tc.draw(gs::vecs(gs::floats().allow_nan(false)).min_size(1).max_size(200));
+        let n_rows = tc.draw(gs::integers().min_value(1).max_value(vec.len()).filter(|x| vec.len().is_multiple_of(*x)));
+        let n_cols = vec.len() / n_rows;
+
+        assert_eq!(n_rows * n_cols, vec.len());
+
+        let dmat: DMatrix<f32> = DMatrix::from_vec(n_rows, n_cols, vec.clone());
+        let ten: Ten = Ten::from(dmat);
+
+        assert_eq!(ten.data, DVector::from_vec(vec));
+        assert_eq!(ten.shape, (n_rows, n_cols).into());
+    }
+
+    #[hegel::test]
+    fn test_vec_of_dmatrix_to_tensor(tc: TestCase) {
+        let vec: Vec<f32> = tc.draw(gs::vecs(gs::floats().allow_nan(false)).min_size(1).max_size(2000));
+        let nrows = tc.draw(gs::integers().min_value(1).max_value(vec.len()).filter(|x| vec.len().is_multiple_of(*x)));
+        let ncols = tc.draw(gs::integers().min_value(1).max_value(vec.len()).filter(|x| (vec.len()/nrows).is_multiple_of(*x)));
+        let channels= vec.len() / (nrows*ncols);
+
+        assert_eq!(nrows * ncols * channels, vec.len());
+
+        let mut to_convert: Vec<DMatrix<f32>> = Vec::new();
+        for channel in 0..channels {
+            to_convert.push(DMatrix::from_iterator(nrows, ncols, (0..nrows*ncols).map(|x| vec[x+(channel*nrows*ncols)])));
+        }
+        let ten: Ten = Ten::from(to_convert);
+
+        assert_eq!(ten.data, DVector::from_vec(vec));
+        assert_eq!(ten.shape, (nrows, ncols, channels).into());
     }
 }
